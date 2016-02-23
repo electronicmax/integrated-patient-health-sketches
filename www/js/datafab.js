d163aa4d0b6d62487fa9b1f49fdaf499b745580a
electronic Max <max@hip.cat>
2016-02-23 00:35:33 +0000

    biasing according to mcgs

--- www/js/datafab.js
+++ www/js/datafab.js
@@ -1,7 +1,17 @@
-/* globals moment, angular, _ */
+/* globals moment, angular, _, d3*/
 
 angular.module('pchealth')
 	.service('datafab', function() {
+
+		var dinner = {
+			'chicken roast':50,
+			'beef roast':20,
+			'spinach casserole':200,
+			'bolognese':50,
+			'chard':150,
+			'broccoli':180
+		};
+
 		var this_ = {
 			createTimeSeries : (start, end) => {
 				if (start && end && start.valueOf() >= end.valueOf()) { throw new Error("start has to be before end"); }
@@ -20,27 +30,48 @@ angular.module('pchealth')
 			slidingRandom:(base, field, variance, min, max) => { 
 				var last = min + (max-min)*Math.random();
 				base.map((x) => {
-					x[field] = last+variance*(Math.random()-0.5); // Math.min(Math.max(min, last + variance*Math.random()), max);
+					x[field] = Math.max(min,Math.min(max, last+variance*(Math.random()-0.5))); // Math.min(Math.max(min, last + variance*Math.random()), max);
 					last = x[field];
 				});
 				return base;
 			},
 			addINRs: (base) => {
-				return this_.slidingRandom(base, 'inr', 0.5, 0.5, 4.0); // base.map((x) => _.extend(x,  { inr : 1.5+Math.random() }));
+				return this_.slidingRandom(base, 'inr', 0.1, 0.5, 4.0); // base.map((x) => _.extend(x,  { inr : 1.5+Math.random() }));
+			},
+			addINRBiased: (base) => {
+				var min = 0.8, max = 3.5,
+					last = min + (max-min)*Math.random(),
+					ext = d3.extent(_.values(dinner)),
+					stdnormal = _.keys(dinner).reduce((arr, k) => { 
+						var avg = (ext[1]+ext[0])/2;
+						console.log('stdnormal ', k, ext, avg, dinner[k]);
 
-				// return base.map((x) => _.extend(x,  { inr : 1.5+Math.random() }));
+						arr[k] = (dinner[k]-avg)/(ext[1]-ext[0]);
+						return arr;
+					},{}),
+					variance = 0.1;
+				console.log('stdnormal' , stdnormal);
+				base.map((x) => {
+					var bias = stdnormal[x.dinner];
+					console.info(' dinner ', x.dinner, bias);
+
+					x.inr = Math.max(min,Math.min(max, last+variance*(Math.random()-0.5 - 5*bias))); // Math.min(Math.max(min, last + variance*Math.random()), max);
+					last = x.inr;
+				});
+				return base;
 			},
 			addChoice: (base, field, choices) => {
+				console.log('choices ', choices);
 				var choose = () => choices[Math.floor(choices.length*Math.random())];
 				base.map((x) => { x[field] = choose(); });
 				return base;
 			},
 			addDinner:(base) => {
-				var dinnerOpts = ['chicken roast','beef roast','spinach casserole','bolognese'];
-				return this_.addChoice(base,'dinner',dinnerOpts);
+				console.log('dinner choices ', _.keys(dinner));
+				return this_.addChoice(base,'dinner',_.keys(dinner));
 			},
 			test: () => {
-				return this_.addDinner(this_.addINRs(this_.createBase()));
+				return this_.addINRBiased(this_.addDinner(this_.createBase()));
 			}
 		};
 		window.fab = this_;
